8e9ed0336987fbd2ce0bef7ed501447a54ab190b
B1nha <128157932+B1nha@users.noreply.github.com>
2023-11-05 21:37:53 -0300

    rotas

--- upsideCakes/AppUpsideCakes/src/app/app-routing.module.ts
+++ upsideCakes/AppUpsideCakes/src/app/app-routing.module.ts
@@ -7,7 +7,20 @@ import { CadastrarProdutoComponent } from './components/componentesProduto/cadas
 import { ListarProdutosComponent } from './components/componentesProduto/listar-produtos/listar-produtos.component';
 import { DeletarProdutoComponent } from './components/componentesProduto/deletar-produto/deletar-produto.component';
 import { AlterarProdutoComponent } from './components/componentesProduto/alterar-produto/alterar-produto.component';
-import { ListarGerentesComponent } from './components/gerenteComponent/listar-gerentes/listar-gerentes.component';
+
+//Imports componentes de Gerente
+import { CadastrarGerentesComponent } from './components/componentesGerente/cadastrar-gerente/cadastrar-gerente.component';
+import { ListarGerentesComponent } from './components/componentesGerente/listar-gerentes/listar-gerentes.component';
+import { DeletarGerenteComponent } from './components/componentesGerente/deletar-gerente/deletar-gerente.component';
+import { AlterarGerenteComponent } from './components/componentesGerente/alterar-gerente/alterar-gerente.component';
+
+//Imports componentes de Pagamento
+/*
+import { CadastrarPagamentosComponent } from './components/componentesPagamento/cadastrar-pagamento/cadastrar-pagamento.component';
+import { ListarPagamentosComponent } from './components/componentesPagamento/listar-pagamentos/listar-pagamentos.component';
+import { DeletarPagamentoComponent } from './components/componentesPagamento/deletar-pagamento/deletar-pagamento.component';
+import { AlterarPagamentoComponent } from './components/componentesPagamento/alterar-pagamento/alterar-pagamento.component';
+*/
 
 const routes: Routes = [
   //Redirenciona a rota vazia '' para rota /home
@@ -21,14 +34,18 @@ const routes: Routes = [
   { path: 'produtos/deletar', component: DeletarProdutoComponent },
 
   //Rotas de Gerentes
-  { path: 'gerentes', component: ListarGerentesComponent},
-  { path: 'gerente/cadastrar', component: CadastrarGerenteComponent },
-  { path: 'gerente/listar', component: ListarGerenteComponent },
+  { path: 'gerente/cadastrar', component: CadastrarGerentesComponent },
+  { path: 'gerente/listar', component: ListarGerentesComponent },
   { path: 'gerente/alterar', component: AlterarGerenteComponent },
   { path: 'gerente/deletar', component: DeletarGerenteComponent },
 
- 
-
+ //Rotas de Pagamento 
+ /*
+  { path: 'pagamento/cadastrar', component: CadastrarPagamentosComponent },
+  { path: 'pagamento/listar', component: ListarPagamentosComponent },
+  { path: 'pagamento/alterar', component: AlterarPagamentoComponent },
+  { path: 'pagamento/deletar', component: DeletarPagamentoComponent },
+ */
 ];
 
 @NgModule({

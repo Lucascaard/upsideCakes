7811fb49c48ccc0f4af40ec872e977a69db536cd
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2023-11-22 20:25:39 -0300

    Updates
    Arrumando combo box de Adicionar Item ao cardápio

--- upsideCakes/AppUpsideCakes/src/app/components/cardapio/add-item-cardapio/add-item-cardapio/add-item-cardapio.component.ts
+++ upsideCakes/AppUpsideCakes/src/app/components/cardapio/add-item-cardapio/add-item-cardapio/add-item-cardapio.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Observer } from 'rxjs';
+import { Cardapio } from 'src/app/models/Cardapio';
 import { Produto } from 'src/app/models/Produto';
 import { CardapiosService } from 'src/app/services/Cardapio/cardapios.service';
+import { ProdutosService } from 'src/app/services/produtos.service';
 
 @Component({
   selector: 'app-add-item-cardapio',
@@ -11,26 +14,89 @@ import { CardapiosService } from 'src/app/services/Cardapio/cardapios.service';
 })
 export class AddItemCardapioComponent {
 
+  produtoSelecionado: Number | undefined;
+  produtos: Array<Produto> | undefined;
+  cardapioSelecionado: Number | undefined;
+  cardapios: Array<Cardapio> | undefined;
   formulario: any;
   tituloFormulario: string = '';
 
-  constructor(private cardapiosService: CardapiosService, private router: Router) { }
+  constructor(private cardapiosService: CardapiosService, private produtosService: ProdutosService, private router: Router) { }
 
   ngOnInit(): void {
-    this.tituloFormulario = "Adicionar item ao Cardapio";
+    this.tituloFormulario = "Adicionar item ao cardápio";
+
+    this.cardapiosService.listar().subscribe(cardapios => {
+      this.cardapios = cardapios;
+      if (this.cardapios && this.cardapios.length > 0) {
+        this.cardapioSelecionado = this.cardapios[0].id;
+        this.formulario.get('cardapioSelecionado')?.setValue(this.cardapios[0].id);
+      }
+    })
+
+    this.produtosService.listar().subscribe(produtos => {
+      this.produtos = produtos;
+      if (this.produtos && this.produtos.length > 0) {
+        this.produtoSelecionado = this.produtos[0].id;
+        this.formulario.get('produtoSelecionado')?.setValue(this.produtos[0].id);
+      }
+    })
+
     this.formulario = new FormGroup({
-      id: new FormControl(null),
-      idCardapio: new FormControl(null)
-    });
+      produtoSelecionado: new FormControl(null),
+      cardapioSelecionado: new FormControl(null)
+      });
+  }
+
+  selecionarProduto(event: any) {
+    const selectedValue = event.target.value;
+    if (selectedValue) {
+      this.produtoSelecionado = parseInt(selectedValue, 10);
+      console.log('Produto selecionado:', this.produtoSelecionado);
+    } else {
+      console.log('Valor selecionado é inválido:', selectedValue);
+    }
+  }
+
+  selecionarCardapio(event: any) {
+    const selectedValue = event.target.value;
+    if (selectedValue) {
+      this.cardapioSelecionado = parseInt(selectedValue, 10);
+      console.log('Cardápio selecionado:', this.cardapioSelecionado);
+    } else {
+      console.log('Valor selecionado é inválido:', selectedValue);
+    }
   }
 
   enviarFormulario(): void {
-    const idCardapio = this.formulario.get('idCardapio').value;
-    const id = this.formulario.get('id').value;
+    if (!this.produtoSelecionado) {
+      alert('Produto selecionado não encontrado.');
+      return;
+    }
 
-    console.log(idCardapio)
-    console.log(id)
-    this.cardapiosService.addItem(id, idCardapio).subscribe(result => {
+    if (!this.cardapioSelecionado) {
+      alert('Cardápio selecionado não encontrado.');
+      return;
+    }
+
+    const produtoSelecionado = this.produtos?.find(produto => produto.id === this.produtoSelecionado);
+
+    const cardapioSelecionado = this.cardapios?.find(cardapio => cardapio.id === this.cardapioSelecionado);
+
+    if (!produtoSelecionado) {
+      alert('Produto não encontrado.');
+      return;
+    }
+  
+    if (!cardapioSelecionado) {
+      alert('Cardápio não encontrado.');
+      return;
+    }
+
+    console.log(cardapioSelecionado);
+    console.log(produtoSelecionado);
+
+    this.cardapiosService.addItem(produtoSelecionado?.id, cardapioSelecionado?.id).subscribe(result => {
       alert('Item adicionado com sucesso.');
     });
   }
--- upsideCakes/AppUpsideCakes/src/app/components/filiais/alterar-filial/alterar-filial.component.ts
+++ upsideCakes/AppUpsideCakes/src/app/components/filiais/alterar-filial/alterar-filial.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Observer } from 'rxjs';
 import { Filial } from 'src/app/models/Filial';
 import { FiliaisService } from 'src/app/services/Filial/filiais.service';
 
@@ -11,6 +12,8 @@ import { FiliaisService } from 'src/app/services/Filial/filiais.service';
 })
 export class AlterarFilialComponent {
 
+  filialSelecionada: Number | undefined;
+  filiais: Array<Filial> | undefined;
   formulario: any;
   tituloFormulario: string = '';
 
@@ -18,17 +21,64 @@ export class AlterarFilialComponent {
 
   ngOnInit(): void {
     this.tituloFormulario = "Alterar dados de Filial";
+
+    this.filiaisService.listar().subscribe(filiais => {
+      this.filiais = filiais;
+      if (this.filiais && this.filiais.length > 0) {
+        this.filialSelecionada = this.filiais[0].id;
+        this.formulario.get('filialSelecionada')?.setValue(this.filiais[0].id);
+      }
+    })
+
     this.formulario = new FormGroup({
-      id: new FormControl(null),
+      filialSelecionada: new FormControl(null),
       cep: new FormControl(null),
       cidade: new FormControl(null),
       rua: new FormControl(null)
       });
   }
 
+  selecionarFilial(event: any) {
+    const selectedValue = event.target.value;
+    if (selectedValue) {
+      this.filialSelecionada = parseInt(selectedValue, 10);
+      console.log('Filial selecionada:', this.filialSelecionada);
+    } else {
+      console.log('Valor selecionado é inválido:', selectedValue);
+    }
+  }
+
   enviarFormulario(): void {
-    const filial : Filial = this.formulario.value;
-    this.filiaisService.alterar(filial).subscribe(result => {
+
+    if (this.filialSelecionada === undefined || this.filialSelecionada === null) {
+      alert('Nenhuma filial selecionada.');
+      return;
+    }
+
+    const filialSelecionada = this.filiais?.find(filial => filial.id === this.filialSelecionada);
+
+    if (!filialSelecionada) {
+      alert('Filial selecionada não encontrada.');
+      return;
+    }
+
+    filialSelecionada.cep = this.formulario.get('cep')?.value;
+    filialSelecionada.rua = this.formulario.get('rua')?.value;
+    filialSelecionada.cidade = this.formulario.get('cidade')?.value;
+
+    const observer: Observer<Filial> = {
+      next(_result): void {
+        alert('Filial alterada com sucesso.');
+      },
+      error(error): void {
+        console.log(error);
+        alert('Erro ao alterar!');
+      },
+      complete(): void {
+      },
+    };
+
+    this.filiaisService.alterar(filialSelecionada).subscribe(result => {
     alert('Filial alterada com sucesso.');
     })
     }
